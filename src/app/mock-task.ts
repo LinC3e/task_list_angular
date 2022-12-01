f99bc2378901b73aa9df4ae41fa145c5d0d3c211
MaTu <ivancostilla17@gmail.com>
2022-12-01 15:01:30 -0300

    Add mock's task

---
+++ src/app/mock-task.ts
@@ -0,0 +1,23 @@
+import { Task } from "./task"
+export const task_list: Task [] = [ 
+    {
+        id: 1,
+        title_task: "Terminar module 1 de Angular",
+        day: "12 Dec",
+        reminder: true
+    },
+
+    {
+        id: 2,
+        title_task: "Mejorar interfaz de header",
+        day: "24 Dec",
+        reminder: true
+    },
+
+    {
+        id: 3,
+        title_task: "Commitear en Git",
+        day: "Todos los dias",
+        reminder: true
+    }
+]
---
+++ src/app/task.ts
@@ -0,0 +1,6 @@
+export interface Task {
+    id?: number 
+    title_task: string
+    day: string
+    reminder: boolean
+}
